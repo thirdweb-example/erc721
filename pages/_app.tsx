989903608db250bf41d7f6f4fa0ab62874adb1c7
Jarrod Watts <jarrodwatts16@gmail.com>
2022-05-17 20:57:46 +1000

    Add Next head

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import type { AppProps } from "next/app";
 import { ChainId, ThirdwebProvider } from "@thirdweb-dev/react";
 import "../styles/globals.css";
+import Head from "next/head";
 
 // This is the chainId your dApp will work on.
 const activeChainId = ChainId.Mumbai;
@@ -8,6 +9,18 @@ const activeChainId = ChainId.Mumbai;
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThirdwebProvider desiredChainId={activeChainId}>
+      <Head>
+        <title>thirdweb NFT Drop Minting Customizable Page</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta
+          name="description"
+          content="Learn How To Use Thirdweb's NFT Drop contract and create a customizable NFT Drop minting page"
+        />
+        <meta
+          name="keywords"
+          content="Thirdweb, thirdweb NFT drop, how to make thirdweb nft drop, how to make nft collection thirdweb"
+        />
+      </Head>
       <Component {...pageProps} />
     </ThirdwebProvider>
   );
