ed3ae10ac87d7dfae9df211bd31df3c2e8dcd76e
Nacho Iacovino <nacho.iacovino@gmail.com>
2023-02-23 20:01:10 -0800

    Initial commit from npx thirdweb create

---
+++ src/App.tsx
@@ -0,0 +1,49 @@
+import { ConnectWallet } from "@thirdweb-dev/react";
+import "./styles/Home.css";
+
+export default function Home() {
+  return (
+    <div className="container">
+      <main className="main">
+        <h1 className="title">
+          Welcome to <a href="https://thirdweb.com/">thirdweb</a>!
+        </h1>
+
+        <p className="description">
+          Get started by configuring your desired network in{" "}
+          <code className="code">src/main.tsx</code>, then modify the{" "}
+          <code className="code">src/App.tsx</code> file!
+        </p>
+
+        <div className="connect">
+          <ConnectWallet />
+        </div>
+
+        <div className="grid">
+          <a href="https://portal.thirdweb.com/" className="card">
+            <h2>Portal &rarr;</h2>
+            <p>
+              Guides, references and resources that will help you build with
+              thirdweb.
+            </p>
+          </a>
+
+          <a href="https://thirdweb.com/dashboard" className="card">
+            <h2>Dashboard &rarr;</h2>
+            <p>
+              Deploy, configure and manage your smart contracts from the
+              dashboard.
+            </p>
+          </a>
+
+          <a href="https://portal.thirdweb.com/templates" className="card">
+            <h2>Templates &rarr;</h2>
+            <p>
+              Discover and clone template projects showcasing thirdweb features.
+            </p>
+          </a>
+        </div>
+      </main>
+    </div>
+  );
+}
---
+++ src/main.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App";
+import { ThirdwebProvider } from "@thirdweb-dev/react";
+import "./styles/globals.css";
+
+// This is the chain your dApp will work on.
+// Change this to the chain your app is built for.
+// You can also import additional chains from `@thirdweb-dev/chains` and pass them directly.
+const activeChain = "ethereum";
+
+const container = document.getElementById("root");
+const root = createRoot(container!);
+root.render(
+  <React.StrictMode>
+    <ThirdwebProvider activeChain={activeChain}>
+      <App />
+    </ThirdwebProvider>
+  </React.StrictMode>
+);
