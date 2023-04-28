c0d3c7f0ef84f7e7f61a0388d2b787beb9a82dd8
Bankk <106103625+BankkRoll@users.noreply.github.com>
2023-04-27 23:31:00 -0700

    Compiled fixes  (#24)
    * Add files via upload
    
    * Update yourDetails.ts
    
    update contract to correct contract
    
    * Update README.md
    
    * Update index.tsx
    
    * Update yourDetails.ts
    
    * Update README.md

---
+++ const/yourDetails.ts
@@ -0,0 +1,2 @@
+// Replace this with your contract address
+export const myNftDropContractAddress: string = "0xbC044bc063F4F88e9d52D833c200aE05Ea65FAF9";
--- pages/index.tsx
+++ pages/index.tsx
@@ -18,9 +18,7 @@ import { useMemo, useState } from "react";
 import Timer from "../components/Timer";
 import styles from "../styles/Theme.module.css";
 import { parseIneligibility } from "../utils/parseIneligibility";
-
-// Put Your NFT Drop Contract address from the dashboard here
-const myNftDropContractAddress = "0xbC044bc063F4F88e9d52D833c200aE05Ea65FAF9";
+import { myNftDropContractAddress } from "../const/yourDetails";
 
 const Home: NextPage = () => {
   const { contract: nftDrop } = useContract(myNftDropContractAddress);
