b754024dde09185f6a5c3690d10f550dd43bccf0
Bankk <106103625+BankkRoll@users.noreply.github.com>
2023-04-27 21:38:10 -0400

    Update yourDetails.ts

--- const/yourDetails.ts
+++ const/yourDetails.ts
@@ -1,2 +1,2 @@
 // Replace this with your contract address
-export const contractAddress: string = "0xbC044bc063F4F88e9d52D833c200aE05Ea65FAF9";
+export const myNftDropContractAddress: string = "0xbC044bc063F4F88e9d52D833c200aE05Ea65FAF9";
